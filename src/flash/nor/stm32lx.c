9afb8b4fd9a50541c4c2b5327531418c489b6de4
tang <kevin.tang@ac6.fr>
2015-09-28 12:03:22 +0200

    Support L011 - L071  Modifications qui seront validées : 	modifié:         src/flash/nor/stm32lx.c

--- src/flash/nor/stm32lx.c
+++ src/flash/nor/stm32lx.c
@@ -139,7 +139,10 @@ static const struct stm32lx_rev stm32_416_revs[] = {
 	{ 0x1000, "A" }, { 0x1008, "Y" }, { 0x1038, "W" }, { 0x1078, "V" },
 };
 static const struct stm32lx_rev stm32_417_revs[] = {
-	{ 0x1000, "A" }, { 0x1008, "Z" },
+	{ 0x1000, "A" }, { 0x1008, "Z" }, { 0x1003, "Y" },
+};
+static const struct stm32lx_rev stm32_425_revs[] = {
+	{ 0x1000, "A" }, { 0x2000, "B" },
 };
 static const struct stm32lx_rev stm32_425_revs[] = {
 	{ 0x1000, "A" },
@@ -159,6 +162,12 @@ static const struct stm32lx_rev stm32_437_revs[] = {
 static const struct stm32lx_rev stm32_447_revs[] = {
 	{ 0x1000, "A" },
 };
+static const struct stm32lx_rev stm32_447_revs[] = {
+	{ 0x1000, "A" }, { 0x2000, "B" },
+};
+static const struct stm32lx_rev stm32_457_revs[] = {
+	{ 0x1000, "A" },
+};
 
 static const struct stm32lx_part_info stm32lx_parts[] = {
 	{
@@ -172,18 +181,6 @@ static const struct stm32lx_part_info stm32lx_parts[] = {
 		.has_dual_banks		= false,
 		.flash_base			= 0x40023C00,
 		.fsize_base			= 0x1FF8004C,
-	},
-	{
-		.id					= 0x417,
-		.revs				= stm32_417_revs,
-		.num_revs			= ARRAY_SIZE(stm32_417_revs),
-		.device_str			= "STM32L0xx (Cat.3)",
-		.page_size			= 128,
-		.pages_per_sector	= 32,
-		.max_flash_size_kb	= 64,
-		.has_dual_banks		= false,
-		.flash_base			= 0x40022000,
-		.fsize_base			= 0x1FF8007C,
 	},
 	{
 		.id					= 0x425,
@@ -247,6 +244,55 @@ static const struct stm32lx_part_info stm32lx_parts[] = {
 		.has_dual_banks		= true,
 		.flash_base			= 0x40023C00,
 		.fsize_base			= 0x1FF800CC,
+	},
+	{
+		.id					= 0x447,
+		.revs				= stm32_447_revs,
+		.num_revs			= ARRAY_SIZE(stm32_447_revs),
+		.device_str			= "STM32L0xx (Cat.5)",
+		.page_size			= 128,
+		.pages_per_sector	= 32,
+		.max_flash_size_kb	= 192,
+		.first_bank_size_kb	= 96,
+		.has_dual_banks		= true,
+		.flash_base			= 0x40022000,
+		.fsize_base			= 0x1FF8007C,
+	},
+	{
+		.id					= 0x457,
+		.revs				= stm32_457_revs,
+		.num_revs			= ARRAY_SIZE(stm32_457_revs),
+		.device_str			= "STM32L0xx (Cat.1)",
+		.page_size			= 128,
+		.pages_per_sector	= 32,
+		.max_flash_size_kb	= 16,
+		.has_dual_banks		= false,
+		.flash_base			= 0x40022000,
+		.fsize_base			= 0x1FF8007C,
+	},
+	{
+		.id					= 0x425,
+		.revs				= stm32_425_revs,
+		.num_revs			= ARRAY_SIZE(stm32_425_revs),
+		.device_str			= "STM32L0xx (Cat.2)",
+		.page_size			= 128,
+		.pages_per_sector	= 32,
+		.max_flash_size_kb	= 32,
+		.has_dual_banks		= false,
+		.flash_base			= 0x40022000,
+		.fsize_base			= 0x1FF8007C,
+	},
+	{
+		.id					= 0x417,
+		.revs				= stm32_417_revs,
+		.num_revs			= ARRAY_SIZE(stm32_417_revs),
+		.device_str			= "STM32L0xx (Cat.3)",
+		.page_size			= 128,
+		.pages_per_sector	= 32,
+		.max_flash_size_kb	= 64,
+		.has_dual_banks		= false,
+		.flash_base			= 0x40022000,
+		.fsize_base			= 0x1FF8007C,
 	},
 	{
 		.id					= 0x447,
@@ -383,7 +429,7 @@ static int stm32lx_write_half_pages(struct flash_bank *bank, const uint8_t *buff
 	struct stm32lx_flash_bank *stm32lx_info = bank->driver_priv;
 
 	uint32_t hp_nb = stm32lx_info->part_info->page_size / 2;
-	uint32_t buffer_size = 16384;
+	uint32_t buffer_size = stm32lx_info->part_info->page_size*8;
 	struct working_area *write_algorithm;
 	struct working_area *source;
 	uint32_t address = bank->base + offset;
@@ -393,7 +439,7 @@ static int stm32lx_write_half_pages(struct flash_bank *bank, const uint8_t *buff
 
 	int retval = ERROR_OK;
 
-	/* see contib/loaders/flash/stm32lx.S for src */
+	/* see contrib/loaders/flash/stm32lx.S for src */
 
 	static const uint8_t stm32lx_flash_write_code[] = {
 		/* write_word: */
@@ -410,7 +456,6 @@ static int stm32lx_write_half_pages(struct flash_bank *bank, const uint8_t *buff
 		0xf8, 0xd3,             /* bcc write_word */
 		0x00, 0xbe,             /* bkpt 0 */
 	};
-
 	/* Make sure we're performing a half-page aligned write. */
 	if (count % hp_nb) {
 		LOG_ERROR("The byte count must be %" PRIu32 "B-aligned but count is %" PRIi32 "B)", hp_nb, count);
@@ -597,7 +642,7 @@ static int stm32lx_write(struct flash_bank *bank, const uint8_t *buffer,
 		return retval;
 
 	/* first we need to write any unaligned head bytes upto
-	 * the next 128 byte page */
+	 * the next half_page byte page */
 
 	if (offset % hp_nb)
 		bytes_remaining = MIN(count, hp_nb - (offset % hp_nb));
